85e8e44d75450c855595272e2cb1863d42d89df7
Massimo Pavoni <maspavoni@gmail.com>
2022-03-29 19:09:22 +0200

    Added TowerColor enum + fix WizardType doc
    - consistency between enumeration description

---
+++ src/main/java/it/polimi/ingsw/javangers/server/model/gameData/enums/TowerColor.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.javangers.server.model.gameData.enums;
+
+/**
+ * Enumeration of the different tower colors available to players.
+ */
+public enum TowerColor {
+    None, White, Black, Gray
+}
--- src/main/java/it/polimi/ingsw/javangers/server/model/gameData/enums/WizardType.java
+++ src/main/java/it/polimi/ingsw/javangers/server/model/gameData/enums/WizardType.java
@@ -1,11 +1,8 @@
 package it.polimi.ingsw.javangers.server.model.gameData.enums;
 
 /**
- * Enumeration of the different types of wizards in the game.
+ * Enumeration of the different types of wizards available to players.
  */
 public enum WizardType {
-    Druid,
-    King,
-    Witch,
-    Sensei
+    Druid, King, Witch, Sensei
 }
