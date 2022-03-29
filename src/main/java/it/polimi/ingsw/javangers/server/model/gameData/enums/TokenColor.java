39aeae3b2a29f4f76d23837f5ec3507f59893ae7
Massimo Pavoni <maspavoni@gmail.com>
2022-03-29 19:11:59 +0200

    Added TokenColor enum
    - (already present, now with values)

--- src/main/java/it/polimi/ingsw/javangers/server/model/gameData/enums/TokenColor.java
+++ src/main/java/it/polimi/ingsw/javangers/server/model/gameData/enums/TokenColor.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.javangers.server.model.gameData.enums;
 
+/**
+ * Enumeration of the different token colors available in the game.
+ */
 public enum TokenColor {
+    YellowElf, BlueUnicorn, GreenFrog, RedDragon, PinkFairy
 }
