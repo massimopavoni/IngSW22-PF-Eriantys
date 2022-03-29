612afd44ec3985027f94a76c12da85f52969c701
Massimo Pavoni <maspavoni@gmail.com>
2022-03-29 19:02:07 +0200

    Added WizardType enum

---
+++ src/main/java/it/polimi/ingsw/javangers/server/model/gameData/enums/WizardType.java
@@ -0,0 +1,11 @@
+package it.polimi.ingsw.javangers.server.model.gameData.enums;
+
+/**
+ * Enumeration of the different types of wizards in the game.
+ */
+public enum WizardType {
+    Druid,
+    King,
+    Witch,
+    Sensei
+}
