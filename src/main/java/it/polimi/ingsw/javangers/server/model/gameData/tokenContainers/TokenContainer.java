af11d65f71ac27886c9524bb8b8a7aee22ed04fc
Massimo Pavoni <maspavoni@gmail.com>
2022-03-30 13:10:01 +0200

    Added TokenContainer abstract base class
    - implementation of class independent methods (base constructor, tokens list get and add, color counts map get)
    - grab tokens method prototype for child classes specific implementation
    - java doc comments

--- src/main/java/it/polimi/ingsw/javangers/server/model/gameData/tokenContainers/TokenContainer.java
+++ src/main/java/it/polimi/ingsw/javangers/server/model/gameData/tokenContainers/TokenContainer.java
@@ -1,4 +1,60 @@
 package it.polimi.ingsw.javangers.server.model.gameData.tokenContainers;
 
+import it.polimi.ingsw.javangers.server.model.gameData.enums.TokenColor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Class representing a container of tokens.
+ */
 public abstract class TokenContainer {
+    /**
+     * List of all the tokens in the container
+     */
+    protected List<TokenColor> tokensList;
+
+    /**
+     * Base constructor for inherited token containers, creates tokens list as empty ArrayList.
+     */
+    public TokenContainer() {
+        this.tokensList = new ArrayList<>();
+    }
+
+    /**
+     * Get a copy of the tokens list.
+     *
+     * @return list of tokens
+     */
+    public List<TokenColor> getTokens() {
+        return new ArrayList<>(this.tokensList);
+    }
+
+    /**
+     * Get map of token colors and number of occurrences of each token color.
+     *
+     * @return map with token colors as keys and number of occurrences as values
+     */
+    public Map<TokenColor, Integer> getColorCounts() {
+        return this.tokensList.stream().collect(Collectors.toMap(color -> color, color -> 1, Integer::sum));
+    }
+
+    /**
+     * Add tokens to tokens list inside container.
+     *
+     * @param tokens list of tokens to add
+     */
+    public void addTokens(List<TokenColor> tokens) {
+        this.tokensList.addAll(tokens);
+    }
+
+    /**
+     * Grab tokens from container with class specific condition and logic.
+     *
+     * @param condition condition to grab tokens
+     * @return list of grabbed tokens
+     */
+    public abstract List<TokenColor> grabTokens(Object condition);
 }
