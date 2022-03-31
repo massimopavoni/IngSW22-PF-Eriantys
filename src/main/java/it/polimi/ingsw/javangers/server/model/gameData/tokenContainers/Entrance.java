b98154fe4e708194a1f9a370e2ca24d1bcd4bb62
thomasscarinzi <thomas.scarinzi@mail.polimi.it>
2022-03-31 15:05:52 +0200

    Entrance and Hall classes
    -Implementation of the grabTokens method in Entrance and Hall classes

---
+++ src/main/java/it/polimi/ingsw/javangers/server/model/gameData/tokenContainers/Entrance.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.javangers.server.model.gameData.tokenContainers;
+
+import it.polimi.ingsw.javangers.server.model.gameData.enums.TokenColor;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Entrance extends TokenContainer<List<TokenColor>> {
+    /**
+     * Method used to grab the tokens that will be moved to another token container
+     *
+     * @param condition List of tokens that have to be grabbed
+     * @return list of tokens grabbed if they are present in the list or an empty list if they aren't
+     */
+    @Override
+    public List<TokenColor> grabTokens(List<TokenColor> condition) {
+        if (this.tokensList.containsAll(condition)) {
+            this.tokensList.remove(condition);
+            return condition;
+        }
+        return Collections.emptyList();
+    }
+}
