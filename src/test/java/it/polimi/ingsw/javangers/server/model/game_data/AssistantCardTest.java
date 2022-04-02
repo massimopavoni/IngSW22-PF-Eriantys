34c1f8ec67e470414c240f0b24b78cdc4ebfc11e
Massimo Pavoni <maspavoni@gmail.com>
2022-04-02 13:09:28 +0200

    Assistant card tests, just constructor, in truth

---
+++ src/test/java/it/polimi/ingsw/javangers/server/model/game_data/AssistantCardTest.java
@@ -0,0 +1,22 @@
+package it.polimi.ingsw.javangers.server.model.game_data;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AssistantCardTest {
+    AssistantCard assistantCard;
+
+    @Test
+    @DisplayName("Test constructor")
+    void AssistantCard_constructor() {
+        assistantCard = new AssistantCard("Cheetah", 1, 1);
+        assertAll(
+                () -> assertEquals("Cheetah", assistantCard.getName()),
+                () -> assertEquals(1, assistantCard.getValue()),
+                () -> assertEquals(1, assistantCard.getSteps())
+        );
+    }
+}
