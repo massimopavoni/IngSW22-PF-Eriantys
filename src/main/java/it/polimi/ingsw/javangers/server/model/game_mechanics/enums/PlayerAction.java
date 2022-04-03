c724bf4147547affcb590125ed3428d6b806b481
Massimo Pavoni <maspavoni@gmail.com>
2022-04-03 21:35:23 +0200

    Added PlayerAction enum

--- src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/enums/PlayerAction.java
+++ src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/enums/PlayerAction.java
@@ -1,4 +1,31 @@
 package it.polimi.ingsw.javangers.server.model.game_mechanics.enums;
 
+/**
+ * Enumeration of the different player actions available in the game, used also for game phases in engine.
+ */
 public enum PlayerAction {
+    /**
+     * Fill clouds action.
+     */
+    FILL_CLOUDS,
+    /**
+     * Play assistant card action.
+     */
+    PLAY_ASSISTANT_CARD,
+    /**
+     * Move students action.
+     */
+    MOVE_STUDENTS,
+    /**
+     * Move mother nature action.
+     */
+    MOVE_MOTHER_NATURE,
+    /**
+     * Choose cloud action.
+     */
+    CHOOSE_CLOUD,
+    /**
+     * Activate character card special action.
+     */
+    ACTIVATE_CHARACTER_CARD
 }
