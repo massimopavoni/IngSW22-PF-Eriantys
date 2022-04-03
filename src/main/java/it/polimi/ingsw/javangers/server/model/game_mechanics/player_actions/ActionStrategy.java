0aad1892c0aa1f5b643961f81475ff802f2dc018
Domenico Pittari <domenico.pittari@mail.polimi.it>
2022-04-03 15:51:26 +0200

    Added ActionStrategy interface

--- src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/ActionStrategy.java
+++ src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/ActionStrategy.java
@@ -1,4 +1,18 @@
 package it.polimi.ingsw.javangers.server.model.game_mechanics.player_actions;
 
-public abstract class ActionStrategy {
+import it.polimi.ingsw.javangers.server.model.game_data.GameState;
+
+/**
+ * Player actions interface.
+ */
+public interface  ActionStrategy {
+    /**
+     * Action the player performs.
+     * @param gameState status of the game
+     * @param username nickname of the player
+     * @return true if the action is done, otherwise it fails
+     */
+    public boolean doAction(GameState gameState, String username);
+
+
 }
