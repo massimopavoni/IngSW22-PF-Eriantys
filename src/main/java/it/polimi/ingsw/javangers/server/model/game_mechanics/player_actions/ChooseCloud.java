68c73f7587d05af847c23b1a03dedc2321626691
Massimo Pavoni <maspavoni@gmail.com>
2022-04-06 16:23:28 +0200

    Fix missing parameter comment

--- src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/ChooseCloud.java
+++ src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/ChooseCloud.java
@@ -5,15 +5,16 @@ import it.polimi.ingsw.javangers.server.model.game_data.GameState;
 /**
  * Class representing choose clouds action.
  */
-public class ChooseCloud implements ActionStrategy{
+public class ChooseCloud implements ActionStrategy {
     /**
-     *  Index of the chosen cloud
+     * Index of the chosen cloud
      */
     private final int cloudIndex;
 
     /**
      * Constructor for choose cloud action, initializing cloud index.
-     * @param cloudIndex
+     *
+     * @param cloudIndex index of the chosen cloud
      */
     public ChooseCloud(int cloudIndex) {
         this.cloudIndex = cloudIndex;
@@ -23,11 +24,10 @@ public class ChooseCloud implements ActionStrategy{
      * Action implementation for choosing clouds.
      *
      * @param gameState game state instance
-     * @param username player username
+     * @param username  player username
      */
     @Override
     public void doAction(GameState gameState, String username) {
-        gameState.getPlayerDashboards().get(username).getEntrance()
-                .addTokens(gameState.getCloudsList().get(cloudIndex).grabTokens());
+        gameState.getPlayerDashboards().get(username).getEntrance().addTokens(gameState.getCloudsList().get(cloudIndex).grabTokens());
     }
 }
