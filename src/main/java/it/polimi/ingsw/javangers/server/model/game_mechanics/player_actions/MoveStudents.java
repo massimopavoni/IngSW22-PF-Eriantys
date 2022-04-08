92d3b783d1f74c1e9795daea611114cb17efebc0
Domenico Pittari <domenico.pittari@mail.polimi.it>
2022-04-08 16:36:51 +0200

    Update MoveStudents with GameEngine
    -change from GameState to GameEngine
    -update teacher with provided function

--- src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/MoveStudents.java
+++ src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/MoveStudents.java
@@ -1,9 +1,9 @@
 package it.polimi.ingsw.javangers.server.model.game_mechanics.player_actions;
 
 import it.polimi.ingsw.javangers.server.model.game_data.Archipelago;
-import it.polimi.ingsw.javangers.server.model.game_data.GameState;
 import it.polimi.ingsw.javangers.server.model.game_data.enums.TokenColor;
 import it.polimi.ingsw.javangers.server.model.game_data.token_containers.TokenContainer;
+import it.polimi.ingsw.javangers.server.model.game_mechanics.GameEngine;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +26,7 @@ public class MoveStudents implements ActionStrategy {
      * Constructor for move students action, initializing students to hall
      * and students to islands.
      *
-     * @param studentsToHall students to move in the hall
+     * @param studentsToHall    students to move in the hall
      * @param studentsToIslands students to move on the islands
      */
     public MoveStudents(List<TokenColor> studentsToHall, Map<Integer, List<TokenColor>> studentsToIslands) {
@@ -37,20 +37,22 @@ public class MoveStudents implements ActionStrategy {
     /**
      * Action implementation to move students.
      *
-     * @param gameState game state instance
-     * @param username  player username
+     * @param gameEngine game state instance
+     * @param username   player username
      */
     @Override
-    public void doAction(GameState gameState, String username) {
-        TokenContainer entrance = gameState.getPlayerDashboards().get(username).getEntrance();
+    public void doAction(GameEngine gameEngine, String username) {
+        TokenContainer entrance = gameEngine.getGameState().getPlayerDashboards().get(username).getEntrance();
         List<TokenColor> tokens = new ArrayList<>(this.studentsToHall);
         this.studentsToIslands.values().forEach(tokens::addAll);
         if (!entrance.containsSubList(tokens)) throw new IllegalStateException("Tokens not present in entrance");
-        TokenContainer hall = gameState.getPlayerDashboards().get(username).getHall();
-        hall.addTokens(entrance.extractTokens(this.studentsToHall));
-        Archipelago archipelago = gameState.getArchipelago();
+        gameEngine.getGameState().getPlayerDashboards().get(username).getHall().addTokens(entrance.extractTokens(this.studentsToHall));
+        Archipelago archipelago = gameEngine.getGameState().getArchipelago();
         for (Map.Entry<Integer, List<TokenColor>> entry : this.studentsToIslands.entrySet()) {
             archipelago.getIslands().get(entry.getKey()).getTokenContainer().addTokens(entrance.extractTokens(entry.getValue()));
         }
+        gameEngine.changeTeachersPower();
     }
+
+
 }
