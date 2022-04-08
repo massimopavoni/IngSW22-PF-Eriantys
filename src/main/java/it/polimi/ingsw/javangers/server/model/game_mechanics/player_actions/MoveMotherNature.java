8bf9b6fbd4ae28ed7d8cd9f5751751970507963c
Massimo Pavoni <maspavoni@gmail.com>
2022-04-08 16:17:47 +0200

    Move mother nature player action
    - implementation of do action with moving mother nature and calling game engine island power update

---
+++ src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/MoveMotherNature.java
@@ -0,0 +1,47 @@
+package it.polimi.ingsw.javangers.server.model.game_mechanics.player_actions;
+
+import it.polimi.ingsw.javangers.server.model.game_data.Archipelago;
+import it.polimi.ingsw.javangers.server.model.game_data.PlayerDashboard;
+import it.polimi.ingsw.javangers.server.model.game_mechanics.GameEngine;
+
+import java.util.Map;
+
+/**
+ * Class representing the action of moving mother nature.
+ */
+public class MoveMotherNature implements ActionStrategy {
+    /**
+     * Requested number of steps to move.
+     */
+    public final int steps;
+
+    /**
+     * Constructor for move mother nature action, initializing the number of steps to move.
+     *
+     * @param steps number of steps to move
+     */
+    public MoveMotherNature(int steps) {
+        this.steps = steps;
+    }
+
+    /**
+     * Action implementation for moving mother nature
+     *
+     * @param gameEngine game engine instance
+     * @param username   player username
+     */
+    @Override
+    public void doAction(GameEngine gameEngine, String username) {
+        Archipelago archipelago = gameEngine.getGameState().getArchipelago();
+        Map<String, PlayerDashboard> playerDashboards = gameEngine.getGameState().getPlayerDashboards();
+        if (this.steps > playerDashboards.get(username).getLastDiscardedAssistantCard().getValue().getSteps()
+                + gameEngine.getAdditionalMotherNatureSteps())
+            throw new IllegalArgumentException("Illegal number of steps");
+        // Get new mother nature position with cyclic movement
+        int newPosition = (archipelago.getMotherNaturePosition() + this.steps) % archipelago.getIslands().size();
+        archipelago.setMotherNaturePosition(newPosition);
+        // Trigger power changes
+        gameEngine.changeIslandPower(archipelago.getMotherNaturePosition(), username);
+    }
+
+}
