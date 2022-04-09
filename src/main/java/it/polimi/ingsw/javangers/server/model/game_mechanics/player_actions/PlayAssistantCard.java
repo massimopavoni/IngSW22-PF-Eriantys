194dfe94b24c48678e3e6ba08113405e52c78d59
Nicola Cecere <nicola.cecere@mail.polimi.it>
2022-04-09 16:18:57 +0200

    Update PlayAssistantCard class and test
    -upgrade logic of doAction with all cases
    -upgrade test adding missing case

--- src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/PlayAssistantCard.java
+++ src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/PlayAssistantCard.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.javangers.server.model.game_mechanics.player_actions;
 
-import it.polimi.ingsw.javangers.server.model.game_data.GameState;
 import it.polimi.ingsw.javangers.server.model.game_data.PlayerDashboard;
+import it.polimi.ingsw.javangers.server.model.game_mechanics.GameEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Class representing the play assistant card action.
@@ -34,15 +39,29 @@ public class PlayAssistantCard implements ActionStrategy {
     /**
      * Action implementation for playing assistant card.
      *
-     * @param gameState game state instance
-     * @param username  player username
+     * @param gameEngine game engine instance
+     * @param username   player username
      */
     @Override
-    public void doAction(GameState gameState, String username) {
-        PlayerDashboard playerDashboard = gameState.getPlayerDashboards().get(username);
-        if (!playerDashboard.getAssistantCards().containsKey(this.cardName))
+    public void doAction(GameEngine gameEngine, String username) {
+        List<String> notUsableAssistantCard = new ArrayList<>();
+        PlayerDashboard myplayerDashboard = gameEngine.getGameState().getPlayerDashboards().get(username);
+        if (!myplayerDashboard.getAssistantCards().containsKey(this.cardName))
             throw new IllegalStateException("Specified card does not exist");
-        playerDashboard.getDiscardedAssistantCards().put(this.cardName, playerDashboard.getAssistantCards().remove(this.cardName));
+        Map<String, PlayerDashboard> othersPlayerDashboards = gameEngine.getGameState().getPlayerDashboards();
+        othersPlayerDashboards.remove(username);
+        for (Map.Entry<String, PlayerDashboard> playerDashboardEntry : othersPlayerDashboards.entrySet()) {
+            if (myplayerDashboard.getDiscardedAssistantCards().size() < playerDashboardEntry.getValue().getDiscardedAssistantCards().size()) {
+                notUsableAssistantCard.add(playerDashboardEntry.getValue().getLastDiscardedAssistantCard().getKey());
+            }
+        }
+        if (notUsableAssistantCard.contains(this.cardName)) {
+            for (String name : myplayerDashboard.getAssistantCards().keySet()) {
+                if (!notUsableAssistantCard.contains(name))
+                    throw new IllegalStateException("Specified player must play a card different from this turn's discards");
+            }
+        }
+        myplayerDashboard.getDiscardedAssistantCards().put(this.cardName, myplayerDashboard.getAssistantCards().remove(this.cardName));
     }
     //endregion
 }
--- src/test/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/PlayAssistantCardTest.java
+++ src/test/java/it/polimi/ingsw/javangers/server/model/game_mechanics/player_actions/PlayAssistantCardTest.java
@@ -1,10 +1,10 @@
 package it.polimi.ingsw.javangers.server.model.game_mechanics.player_actions;
 
 import it.polimi.ingsw.javangers.server.model.game_data.AssistantCard;
-import it.polimi.ingsw.javangers.server.model.game_data.GameState;
-import it.polimi.ingsw.javangers.server.model.game_data.enums.TokenColor;
+import it.polimi.ingsw.javangers.server.model.game_data.PlayerDashboard;
 import it.polimi.ingsw.javangers.server.model.game_data.enums.TowerColor;
 import it.polimi.ingsw.javangers.server.model.game_data.enums.WizardType;
+import it.polimi.ingsw.javangers.server.model.game_mechanics.GameEngine;
 import org.javatuples.Pair;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,52 +15,85 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayAssistantCardTest {
-    GameState gameState;
-
-    @Test
-    @DisplayName("Test constructor")
-    void PlayAssistantCard_constructor() {
-        PlayAssistantCard playAssistantCard = new PlayAssistantCard("test");
-        assertTrue(true);
-    }
+    GameEngine gameEngine;
 
     @Test
     @DisplayName("Test play a non specified card")
-    void doAction_notSpecifiedCard() throws GameState.GameStateException {
+    void doAction_notSpecifiedCard() throws GameEngine.GameEngineException {
         PlayAssistantCard playAssistantCard = new PlayAssistantCard("test");
-        gameState = new GameState("/it/polimi/ingsw/javangers/server/model/game_data/assistant_cards.json",
+        gameEngine = new GameEngine("/it/polimi/ingsw/javangers/server/model/game_mechanics/game_configurations.json",
                 new HashMap<String, Pair<WizardType, TowerColor>>() {{
                     put("pippo", new Pair<>(WizardType.KING, TowerColor.WHITE));
-                }}, 8, 12, new HashMap<TokenColor, Integer>(), 1);
+                    put("pluto", new Pair<>(WizardType.DRUID, TowerColor.BLACK));
+                }}, false);
         assertThrowsExactly(IllegalStateException.class,
-                () -> playAssistantCard.doAction(gameState, "pippo"),
+                () -> playAssistantCard.doAction(gameEngine, "pippo"),
                 "Specified card does not exist");
     }
 
     @Test
     @DisplayName("Test play a specified card")
-    void doAction_SpecifiedCard() throws GameState.GameStateException {
+    void doAction_SpecifiedCard() throws GameEngine.GameEngineException {
         PlayAssistantCard playAssistantCard = new PlayAssistantCard("cheetah");
-        gameState = new GameState("/it/polimi/ingsw/javangers/server/model/game_data/assistant_cards.json",
+        gameEngine = new GameEngine("/it/polimi/ingsw/javangers/server/model/game_mechanics/game_configurations.json",
                 new HashMap<String, Pair<WizardType, TowerColor>>() {{
                     put("pippo", new Pair<>(WizardType.KING, TowerColor.WHITE));
-                }}, 8, 12, new HashMap<TokenColor, Integer>(), 1);
-        playAssistantCard.doAction(gameState, "pippo");
-        Map<String, AssistantCard> cardPlayed = gameState.getPlayerDashboards().get("pippo").getDiscardedAssistantCards();
+                    put("pluto", new Pair<>(WizardType.DRUID, TowerColor.BLACK));
+                }}, false);
+        playAssistantCard.doAction(gameEngine, "pippo");
+        Map<String, AssistantCard> cardPlayed = gameEngine.getGameState().getPlayerDashboards().get("pippo").getDiscardedAssistantCards();
         assertNotNull(cardPlayed.get("cheetah"));
     }
 
     @Test
     @DisplayName("Test play a specified card and is not discard the same")
-    void doAction_notDiscardTheSame() throws GameState.GameStateException {
+    void doAction_notDiscardTheSame() throws GameEngine.GameEngineException {
         PlayAssistantCard playAssistantCard = new PlayAssistantCard("cheetah");
-        gameState = new GameState("/it/polimi/ingsw/javangers/server/model/game_data/assistant_cards.json",
+        gameEngine = new GameEngine("/it/polimi/ingsw/javangers/server/model/game_mechanics/game_configurations.json",
                 new HashMap<String, Pair<WizardType, TowerColor>>() {{
                     put("pippo", new Pair<>(WizardType.KING, TowerColor.WHITE));
-                }}, 8, 12, new HashMap<TokenColor, Integer>(), 1);
-        playAssistantCard.doAction(gameState, "pippo");
-        Map<String, AssistantCard> cardPlayed = gameState.getPlayerDashboards().get("pippo").getDiscardedAssistantCards();
+                    put("pluto", new Pair<>(WizardType.DRUID, TowerColor.BLACK));
+                }}, false);
+        playAssistantCard.doAction(gameEngine, "pippo");
+        Map<String, AssistantCard> cardPlayed = gameEngine.getGameState().getPlayerDashboards().get("pippo").getDiscardedAssistantCards();
         assertNull(cardPlayed.get("ostrich"));
-
     }
+
+    @Test
+    @DisplayName("Test play a specified already played with other options")
+    void doAction_useCardAlreadyPlayed() throws GameEngine.GameEngineException {
+        gameEngine = new GameEngine("/it/polimi/ingsw/javangers/server/model/game_mechanics/game_configurations.json",
+                new HashMap<String, Pair<WizardType, TowerColor>>() {{
+                    put("pippo", new Pair<>(WizardType.KING, TowerColor.WHITE));
+                    put("pluto", new Pair<>(WizardType.DRUID, TowerColor.BLACK));
+                }}, false);
+        PlayAssistantCard playAssistantCard = new PlayAssistantCard("cheetah");
+        playAssistantCard.doAction(gameEngine, "pippo");
+        assertThrowsExactly(IllegalStateException.class,
+                () -> playAssistantCard.doAction(gameEngine, "pluto"),
+                "Specified player must play a card different from this turn's discards");
+    }
+
+    @Test
+    @DisplayName("Test play a specified already played without other options")
+    void doAction_useCardAlreadyPlayedButWithNoChoice() throws GameEngine.GameEngineException {
+        gameEngine = new GameEngine("/it/polimi/ingsw/javangers/server/model/game_mechanics/game_configurations.json",
+                new HashMap<String, Pair<WizardType, TowerColor>>() {{
+                    put("pippo", new Pair<>(WizardType.KING, TowerColor.WHITE));
+                    put("pluto", new Pair<>(WizardType.DRUID, TowerColor.BLACK));
+                }}, false);
+        String[] cardNames;
+        for (Map.Entry<String, PlayerDashboard> playerDashboardEntry : gameEngine.getGameState().getPlayerDashboards().entrySet()) {
+            cardNames = playerDashboardEntry.getValue().getAssistantCards().keySet().toArray(new String[0]);
+            for (int i = 0; i < 9; i++) {
+                playerDashboardEntry.getValue().getDiscardedAssistantCards().put(cardNames[i],
+                        playerDashboardEntry.getValue().getAssistantCards().remove(cardNames[i]));
+            }
+        }
+        PlayAssistantCard playAssistantCard = new PlayAssistantCard("turtle");
+        playAssistantCard.doAction(gameEngine, "pippo");
+        playAssistantCard.doAction(gameEngine, "pluto");
+        assertEquals("turtle", gameEngine.getGameState().getPlayerDashboards().get("pluto").getLastDiscardedAssistantCard().getKey());
+    }
+
 }
