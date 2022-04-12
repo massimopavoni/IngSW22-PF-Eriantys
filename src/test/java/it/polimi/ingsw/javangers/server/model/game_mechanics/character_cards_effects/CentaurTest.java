8af28e8423226b755caa2fb456f78bf572c38833
thomasscarinzi <thomas.scarinzi@mail.polimi.it>
2022-04-12 09:13:47 +0200

    Update CentaurTest.java
    - fixing code in the test (use of the character card)

--- src/test/java/it/polimi/ingsw/javangers/server/model/game_mechanics/character_cards_effects/CentaurTest.java
+++ src/test/java/it/polimi/ingsw/javangers/server/model/game_mechanics/character_cards_effects/CentaurTest.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.javangers.server.model.game_mechanics.character_cards_ef
 
 import it.polimi.ingsw.javangers.server.model.game_data.enums.TowerColor;
 import it.polimi.ingsw.javangers.server.model.game_data.enums.WizardType;
+import it.polimi.ingsw.javangers.server.model.game_mechanics.CharacterCard;
 import it.polimi.ingsw.javangers.server.model.game_mechanics.GameEngine;
 import org.javatuples.Pair;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,10 +11,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class CentaurTest {
     GameEngine gameEngine;
+    CharacterCard centaur;
 
     @BeforeEach
     void setUp() throws GameEngine.GameEngineException {
@@ -23,12 +25,15 @@ class CentaurTest {
                     put("Thor", new Pair<>(WizardType.KING, TowerColor.BLACK));
                     put("Cap", new Pair<>(WizardType.SENSEI, TowerColor.WHITE));
                 }}, true);
+        centaur = gameEngine.getCharacterCards().get("bard");
     }
 
     @Test
     @DisplayName("Test useEffect of Centaur class for correct behavior")
-    void useEffect_correct(){
-        gameEngine.setEnabledIslandTowers(false);
+    void useEffect_correct() {
+        Centaur centaurEffect = new Centaur();
+        centaur.setEffect(centaurEffect);
+        centaur.activateEffect(gameEngine, "thor");
         assertFalse(gameEngine.getEnabledIslandTowers());
     }
 }
