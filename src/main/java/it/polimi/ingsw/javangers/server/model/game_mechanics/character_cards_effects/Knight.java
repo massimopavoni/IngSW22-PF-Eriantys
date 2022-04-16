40f66ee9c6a47e0cd0f85fede4b420e95b56b455
thomasscarinzi <thomas.scarinzi@mail.polimi.it>
2022-04-16 22:37:01 +0200

    Create Knight + test
    - creation of Knight character card and related test

---
+++ src/main/java/it/polimi/ingsw/javangers/server/model/game_mechanics/character_cards_effects/Knight.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.javangers.server.model.game_mechanics.character_cards_effects;
+
+import it.polimi.ingsw.javangers.server.model.game_mechanics.CharacterCard;
+import it.polimi.ingsw.javangers.server.model.game_mechanics.core.GameEngine;
+
+/**
+ * Class representing the knight character card.
+ */
+public class Knight implements EffectStrategy{
+    //--------------------------------------------------------------------------------------------------------------------------------
+    //region Methods
+    /**
+     * Method called to use the effect of the knight character card.
+     *
+     * @param gameEngine game engine instance
+     * @param username   player username
+     */
+    @Override
+    public void useEffect(GameEngine gameEngine, String username) {
+        CharacterCard knight = gameEngine.getCharacterCards().get(this.getClass().getSimpleName().toLowerCase());
+        gameEngine.setAdditionalPower(knight.getMultipurposeCounter());
+    }
+    //endregion
+}
