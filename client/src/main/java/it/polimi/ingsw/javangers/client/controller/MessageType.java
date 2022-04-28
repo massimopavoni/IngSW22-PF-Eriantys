a3df6f7d00a0b70b5573a2dfc9e80310d4e97db1
Massimo Pavoni <maspavoni@gmail.com>
2022-04-28 15:35:24 +0200

    Update for pom project alpha.2 + message classes on controller

---
+++ client/src/main/java/it/polimi/ingsw/javangers/client/controller/MessageType.java
@@ -0,0 +1,27 @@
+package it.polimi.ingsw.javangers.client.controller;
+
+/**
+ * Enumeration of the different message types.
+ */
+public enum MessageType {
+    /**
+     * Game creation.
+     */
+    CREATE,
+    /**
+     * Player addition to the game.
+     */
+    PLAYER,
+    /**
+     * Game initialization.
+     */
+    START,
+    /**
+     * Player action execution.
+     */
+    ACTION,
+    /**
+     * Error message.
+     */
+    ERROR
+}
--- client/src/main/java/module-info.java
+++ client/src/main/java/module-info.java
@@ -4,4 +4,6 @@ module it.polimi.ingsw.eriantysclient {
 
     opens it.polimi.ingsw.javangers.client.launcher to javafx.fxml;
     exports it.polimi.ingsw.javangers.client.launcher;
+    exports it.polimi.ingsw.javangers.client.controller;
+    opens it.polimi.ingsw.javangers.client.controller to javafx.fxml;
 }
