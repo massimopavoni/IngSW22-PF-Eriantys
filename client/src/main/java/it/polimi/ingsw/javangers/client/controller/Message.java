5b0e2ea7e1bbc2c25e45175a3ef5ed9ebe640fae
Massimo Pavoni <maspavoni@gmail.com>
2022-05-01 19:48:42 +0200

    Test remove module info and message record again

--- client/src/main/java/it/polimi/ingsw/javangers/client/controller/Message.java
+++ client/src/main/java/it/polimi/ingsw/javangers/client/controller/Message.java
@@ -2,58 +2,10 @@ package it.polimi.ingsw.javangers.client.controller;
 
 /**
  * Class representing a network message.
+ *
+ * @param type     Type of the message.
+ * @param username Username of the player sending the message.
+ * @param content  Content of the message.
  */
-public class Message {
-    /**
-     * Type of the message.
-     */
-    private final MessageType type;
-    /**
-     * Username of the player sending the message.
-     */
-    private final String username;
-    /**
-     * Content of the message.
-     */
-    private final String content;
-
-    /**
-     * Constructor for message, initializing message type, username and content.
-     *
-     * @param type     type of the message
-     * @param username username of the player sending the message
-     * @param content  content of the message
-     */
-    public Message(MessageType type, String username, String content) {
-        this.type = type;
-        this.username = username;
-        this.content = content;
-    }
-
-    /**
-     * Get message type.
-     *
-     * @return message type
-     */
-    public MessageType getType() {
-        return this.type;
-    }
-
-    /**
-     * Get message username.
-     *
-     * @return message username
-     */
-    public String getUsername() {
-        return this.username;
-    }
-
-    /**
-     * Get message content.
-     *
-     * @return message content
-     */
-    public String getContent() {
-        return this.content;
-    }
+public record Message(MessageType type, String username, String content) {
 }
