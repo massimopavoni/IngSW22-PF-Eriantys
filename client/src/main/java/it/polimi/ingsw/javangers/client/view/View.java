966b44fccaacfbbce40a2c4f468a5770c084ab47
Massimo Pavoni <maspavoni@gmail.com>
2022-05-02 14:41:33 +0200

    Better view abstract class

--- client/src/main/java/it/polimi/ingsw/javangers/client/view/View.java
+++ client/src/main/java/it/polimi/ingsw/javangers/client/view/View.java
@@ -8,6 +8,26 @@ public abstract class View {
      * Locking object for view update wait.
      */
     private final Object updateLock = new Object();
+    /**
+     * Chosen number of players for the game.
+     */
+    private int exactPlayersNumber;
+    /**
+     * Chosen expert mode flag.
+     */
+    private boolean expertMode;
+    /**
+     * Chosen player username.
+     */
+    private String username;
+    /**
+     * Chosen player wizard type.
+     */
+    private String wizardType;
+    /**
+     * Chosen player tower color
+     */
+    private String towerColor;
 
     /**
      * Method called to unlock the view update wait object.
@@ -17,6 +37,7 @@ public abstract class View {
             this.updateLock.notifyAll();
         }
     }
+
     /**
      * Method called to update the view game information.
      */
