6f65d96a11d5ac2d9a4d7c477bdb66f637db449b
Massimo Pavoni <maspavoni@gmail.com>
2022-05-18 14:43:47 +0200

    Final build client script version (simplified for builds upload instead)

--- client/build_client.py
+++ client/build_client.py
@@ -1,30 +1,10 @@
-from os import pardir
-from os import path
-from shutil import make_archive
-from shutil import rmtree
 from subprocess import call as subprocess_call
 from sys import argv
-from xml.etree import ElementTree
 
-
-def maven_install():
-    distributions = ','.join(argv[1:])
-    print(f"Creating distributions for [{distributions}]")
-    subprocess_call(['mvn', 'install', '-P', f'"{distributions}"'])
-
-def archive():
-    print("Creating distribution archives")
-    tree = ElementTree.parse('pom.xml')
-    root = tree.getroot()
-    client_name = f'Eriantys-{root.find("{http://maven.apache.org/POM/4.0.0}version").text}'
-    client_path = path.join(pardir, 'executables', client_name)
-    make_archive(client_path, 'zip', path.join(pardir, 'executables'), client_name)
-    rmtree(client_path)
 
 def main():
-    maven_install()
-    archive()
-
+    print(f"Creating distributions for [argv[1]}")
+    subprocess_call(['mvn', 'install', '-P', argv[1]])
 
 if __name__ == '__main__':
     main()
